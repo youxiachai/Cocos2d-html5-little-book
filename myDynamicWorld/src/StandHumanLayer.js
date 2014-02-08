bedf0c7083330e883c90dc821704d26afb7befe2
youxiachai <youxiachai@gmail.com>
2014-02-09 02:05:39 +0800

    update

--- myDynamicWorld/src/StandHumanLayer.js
+++ myDynamicWorld/src/StandHumanLayer.js
@@ -11,15 +11,15 @@ var StandHumanLayer = cc.Layer.extend({
 
         // 1  静态
         var humanSprite = cc.Sprite.create(path);
-        humanSprite.setPosition(cc.p(80, 85));
+        humanSprite.setPosition(cc.p(80, 57));
 
         this.addChild(humanSprite);
 
 
         //2  移动
         var humanSprite2 = cc.Sprite.create(path);
-        humanSprite2.setPosition(cc.p(80, 170));
-        var actionTo = cc.MoveTo.create(2, cc.p(300, 170));
+        humanSprite2.setPosition(cc.p(80, 200));
+        var actionTo = cc.MoveTo.create(2, cc.p(300, 200));
         humanSprite2.runAction(cc.Sequence.create(actionTo));
         this.addChild(humanSprite2);
 
